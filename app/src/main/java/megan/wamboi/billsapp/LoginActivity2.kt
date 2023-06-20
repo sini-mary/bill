3428eae7d6404d57bcab4fdcd4c0f5dded02ecd4
wamboimargaret <margarettwamboi@gmail.com>
2023-06-20 08:46:34 +0300

    lOGIN

---
+++ app/src/main/java/megan/wamboi/billsapp/LoginActivity2.kt
@@ -0,0 +1,11 @@
+package megan.wamboi.billsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login2)
+    }
+}
